e1ce93e370457dff5b7c9cc307eb0e5326fdd4fb
Ben Olson <ben.olson@essium.co>
2020-06-06 19:31:00 -0400

    Use Javascript native flatten()

--- simple-oauth.js
+++ simple-oauth.js
@@ -390,7 +390,7 @@
                params.push( _(_.flatten([vs]).sort()).chain().map(function (v) { return [k, v]; }).value() );
             });
 
-         return _.flatten(params, true);
+         return params.flatten(true);
       }
 
    });
